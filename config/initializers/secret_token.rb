8f4039c08664fa39b78b029ff0a7273ddd5a1f80
Van Michael <van.michael.nguyen@gmail.com>
2013-04-09 23:34:32 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarevanApp::Application.config.secret_token = '3b0af7f0bb775a84be4b06bc3893a16d39c671a9c57c944c202570ee174870c5b671d8944ad2f1180d4ffc8e84dcc570725e40259fcd28edc857f90733718548'
